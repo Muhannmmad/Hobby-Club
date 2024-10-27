394052b8ce8f5e6ace2a67fafda1bbb9bf8fb323
Muhammad <muhannmmad@gmail.com>
2024-10-27 21:47:28 +0100

    removed unused photos

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hoppy_club/config/config.dart';
 import 'package:hoppy_club/features/home/widgets/hobbies_card.dart';
 import 'package:hoppy_club/features/profiles/screens/new_mach_screen.dart';
 import 'package:hoppy_club/features/chat/screens/chat_screen.dart';
