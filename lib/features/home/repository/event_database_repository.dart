634dcfd20589986bdc874e1fe3b5865a3fa36244
Muhammad <muhannmmad@gmail.com>
2025-01-28 15:06:50 +0100

    unwanted files removed

--- lib/features/home/repository/event_database_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:hoppy_club/features/home/repository/events.dart';
-
-abstract class EventDatabaseRepository {
-  Future<List<Event>> fetchEvents();
-  Future<void> addEvent(Event event);
-  Future<void> deleteEvent(String title);
-}
